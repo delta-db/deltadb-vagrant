fcc3f3b2ec517a59eb775853f84b80a1fbee857d
Geoffrey Cox <redgeoff@gmail.com>
2015-06-16 20:40:20 -0700

    fix(bootstrap): g++ needed by bcrypt and pg

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 apt-get update
 apt-get install -y nodejs git
 
+# Needed by bcrypt, pg, etc...
+apt-get install -y g++
+
 # Install postgres
 apt-get install -y postgresql libpq-dev
 
